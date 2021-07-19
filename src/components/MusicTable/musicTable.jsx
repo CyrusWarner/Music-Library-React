ed98be57589e0284832eeac5b8c63c6a7914ec66
jamesburgess44 <jamesburgess44@yahoo.com>
2021-07-19 12:21:12 -0600

    organized table data and removed ID field

--- src/components/MusicTable/musicTable.jsx
+++ src/components/MusicTable/musicTable.jsx
@@ -9,22 +9,21 @@ function MusicTable(props) {
                 <table class="ui celled padded table">
                     <thead>
                     <tr>
-                        <th>Id</th>
+                        
                         <th>Album</th>
                         <th>Artist</th>
-                        <th>Release Date</th>
                         <th>Title</th>
+                        <th>Release Date</th>
                         <th>Delete Song</th>
                     </tr>
                     </thead>
                         {props.music.map((song) => {
                             return (
                                 <tr>
-                                    <td>{song.id}</td>
                                     <td>{song.album}</td>
                                     <td>{song.artist}</td>
-                                    <td>{song.release_date}</td>
                                     <td>{song.title}</td>
+                                    <td>{song.release_date}</td>
                                     <td><i className="trash alternate outline icon" style={{color:"red"}} onClick={() => props.deleteSong(song,)}></i></td>
                                 </tr>
                             )
